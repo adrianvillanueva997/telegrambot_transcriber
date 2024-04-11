073bf658500d4c434ec5c0829c776ec42a903cff
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2024-04-11 11:28:49 +0200

    added vosk model and more dependencies

--- src/transcription/transcript.rs
+++ src/transcription/transcript.rs
@@ -1,7 +1,14 @@
 use std::path::Path;
 use std::process::Command;
 
+use rust_embed::RustEmbed;
+use vosk::{Model, Recognizer};
+
 pub const AUDIO: &str = "audio.ogg";
+
+#[derive(RustEmbed)]
+#[folder = "models"]
+struct Models;
 
 pub async fn transcribe_voice_message() -> Result<(), Box<dyn std::error::Error>> {
     if !audio_file_exists() {
@@ -26,4 +33,24 @@ pub async fn transcribe_voice_message() -> Result<(), Box<dyn std::error::Error>
 /// .
 fn audio_file_exists() -> bool {
     Path::new(AUDIO).exists()
+}
+
+fn test() {
+    let samples = [100, -2, 700, 30, 4, 5];
+    // let model_path = Models::get("vosk-model-small-es-0.42.zip");
+    let model_path = "vosk-model-small-es-0.42.zip";
+
+    let model = Model::new(model_path).unwrap();
+    let mut recognizer = Recognizer::new(&model, 16000.0).unwrap();
+
+    recognizer.set_max_alternatives(10);
+    recognizer.set_words(true);
+    recognizer.set_partial_words(true);
+
+    for sample in samples.chunks(100) {
+        recognizer.accept_waveform(sample);
+        println!("{:#?}", recognizer.partial_result());
+    }
+
+    println!("{:#?}", recognizer.final_result().multiple().unwrap());
 }
