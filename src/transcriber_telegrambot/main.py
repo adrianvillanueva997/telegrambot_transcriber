10d95d27ce1ac38d71dbb00d7999ca350c691331
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2024-07-10 13:02:35 +0200

    rollback to python

--- src/transcriber_telegrambot/main.py
+++ src/transcriber_telegrambot/main.py
@@ -1,7 +1,8 @@
+import asyncio
 import os
-import subprocess
 from shutil import ExecError
 
+import anyio
 from loguru import logger
 from prometheus_client import start_http_server
 from telegram import File, Update
@@ -14,30 +15,44 @@ from telegram.ext import (
 )
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def help_command(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
+    if update.message:
+        await update.message.reply_text("Help!")
 
 
 async def transcribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Transcribres the user message."""
-    if hasattr(update.message.voice, "file_id"):
+    if (
+        update.message
+        and update.message.voice
+        and hasattr(update.message.voice, "file_id")
+    ):
         voice_file: File = await context.bot.get_file(update.message.voice.file_id)
         logger.info(update.message.voice)
         await voice_file.download_to_drive("audio.ogg")
         try:
-            output = subprocess.run(
-                ["vosk-transcriber", "-l", "es", "-i", "audio.ogg", "-o", "output.txt"],
-                capture_output=True,
-                text=True,
-                encoding="utf-8",
+            process = await asyncio.create_subprocess_exec(
+                "vosk-transcriber",
+                "-l",
+                "es",
+                "-i",
+                "audio.ogg",
+                "-o",
+                "output.txt",
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE,
             )
-            if output.returncode == 0:
-                with open("output.txt", "r") as file:
-                    content = file.read()
+            stdout, stderr = await process.communicate()
+            output = stdout.decode("utf-8")
+
+            if output == 0:
+                async with await anyio.open_file("output.txt") as file:
+                    content = await file.read()
                     try:
                         await update.message.reply_text(
-                            text=content, reply_to_message_id=update.message.id
+                            text=content,
+                            reply_to_message_id=update.message.id,
                         )
                     except Exception as e:
                         logger.error(e)
@@ -48,7 +63,7 @@ async def transcribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None
 def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token(os.environ["bot_token"]).build()
+    application = Application.builder().token(os.environ["BOT_TOKEN"]).build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("help", help_command))
